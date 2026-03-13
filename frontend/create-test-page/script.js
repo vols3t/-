518abcd74767f555fd0a073451087cfd549a8bb6
Grossbauch <mishukov9042@gmail.com>
2026-03-13 17:52:16 +0500

    feat: разработка интерфейса теста

--- frontend/create-test-page/script.js
+++ frontend/create-test-page/script.js
@@ -20,14 +20,35 @@ document.addEventListener('DOMContentLoaded', function () {
             if (!response.ok) {
                 throw new Error(`Ошибка сервера: ${response.status}`);
             }
-            const result = await response.json();
-            console.log('Ответ от сервера:', result);
+            const questions = await response.json(); // Теперь это массив вопросов
+            console.log('Вопросы:', questions);
+
+            const main = document.querySelector('main');
+            main.innerHTML = `<h2 id="testTopic">Тест по теме: ${topic}</h2><form id="quizForm"></form>`;
+
+            const quizForm = document.getElementById('quizForm');
+            questions.forEach((q, index) => {
+                let optionsHtml = q.options.map((opt, i) => `
+        <label class="option-item">
+            <input type="checkbox" name="q${index}" value="${i}">
+            <span>${opt}</span>
+        </label>
+    `).join('');
+
+                quizForm.innerHTML += `
+        <div class="form-group">
+            <span class="label">${index + 1}. ${q.questionText}</span>
+            ${optionsHtml}
+        </div>
+    `;
+            });
+            quizForm.innerHTML += `<button type="button" class="btn" id="submitBtn">Завершить</button>`;
 
             alert(`Тест по теме "${topic}" успешно создан на сервере!`);
 
         } catch (error) {
             console.log('Ошибка при отправке', error);
-            alert('Не удалось создать тест')
+            alert('Не удалось создать тест');
         }
     })
 })
