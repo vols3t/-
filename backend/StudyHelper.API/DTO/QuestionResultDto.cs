c0a6c5503ee64fb329c3b4a2a1830529a64cbd95
evgeniavolkova <evgeniafil04@gmail.com>
2026-03-20 01:02:04 +0500

    QuestionResultDto сделала более удобный для будущего фронта конструктор

--- backend/StudyHelper.API/DTO/QuestionResultDto.cs
+++ backend/StudyHelper.API/DTO/QuestionResultDto.cs
@@ -4,16 +4,16 @@ public class QuestionResultDto
 {
     public int ID { get; private set; }
 
-    // public string Question { get; private set; }
+    public string Question { get; private set; }
     public List<string> Answers { get; private set; }
     public string CorrectAnswer { get; private set; }
     public string RealAnswer { get; set; }
-    public bool IsCorrectAnswer { get; set; }
+    public bool IsCorrectAnswer { get; set; } = false;
 
-    public QuestionResultDto(int id, string correctAnswer)
+    public QuestionResultDto(int id, string question, string correctAnswer)
     {
         ID = id;
-        // Question = question;
+        Question = question;
         CorrectAnswer = correctAnswer;
     }
 }
