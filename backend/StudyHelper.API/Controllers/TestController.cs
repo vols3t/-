d8ef122fa730442655333263fa43e54f7c2b9e36
evgeniavolkova <evgeniafil04@gmail.com>
2026-03-20 01:00:59 +0500

    просто отрефакторила opt+cmd+L

--- backend/StudyHelper.API/Controllers/TestController.cs
+++ backend/StudyHelper.API/Controllers/TestController.cs
@@ -17,12 +17,13 @@ public class TestController : ControllerBase
 
         // Здесь позже будет вызов к ChatGPT API, пока просто заглушка
         var mockQuestions = new List<object>();
-        for (int i = 1; i <= request.QuestionsCount; i++)
+        for (var i = 1; i <= request.QuestionsCount; i++)
         {
             mockQuestions.Add(new
             {
                 id = i,
-                questionText = $"Вопрос: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae perferendis aliquam officia praesentium voluptatum similique reprehenderit recusandae obcaecati! Ut quaerat cumque laborum amet ducimus similique?",
+                questionText =
+                    $"Вопрос: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae perferendis aliquam officia praesentium voluptatum similique reprehenderit recusandae obcaecati! Ut quaerat cumque laborum amet ducimus similique?",
                 options = new[] { "Вариант А", "Вариант Б", "Вариант В", "Вариант Г" }
             });
         }
