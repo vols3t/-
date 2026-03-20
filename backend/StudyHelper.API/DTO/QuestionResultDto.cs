93654ac9cc1cedf163cdfdc3c75db1063b935d85
evgeniavolkova <evgeniafil04@gmail.com>
2026-03-21 01:35:11 +0500

    чуть поправила бэк, чтобы передавать все ответы на фронт

--- backend/StudyHelper.API/DTO/QuestionResultDto.cs
+++ backend/StudyHelper.API/DTO/QuestionResultDto.cs
@@ -5,7 +5,7 @@ public class QuestionResultDto
     public int ID { get; private set; }
 
     public string Question { get; private set; }
-    public List<string> Answers { get; private set; }
+    public List<string> Answers { get; set; }
     public string CorrectAnswer { get; private set; }
     public string RealAnswer { get; set; }
     public bool IsCorrectAnswer { get; set; } = false;
