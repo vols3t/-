54ce23e6300a82e3f1d3c93a0a037f09390f6ec2
Stepa_Volkov <wdgetso2@gmail.com>
2026-04-13 18:48:41 +0500

    test

--- backend/StudyHelper.API/Program.cs
+++ backend/StudyHelper.API/Program.cs
@@ -21,17 +21,10 @@ builder.Services.AddCors(options =>
     });
 });
 
-if (builder.Environment.IsProduction())
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseNpgsql(connectionString));
-}
-else
-{
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseInMemoryDatabase("StudyHelperDb"));
-}
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("StudyHelperDb"));
+
 
 builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
 builder.Services.AddScoped<ITestService, TestService>();
